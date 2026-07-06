e923079f9b6f5fa7836f7a6b3842eb6150e10007
Arun Kumar <arun@identitidesign.com>
2026-07-06 20:52:19 +0530

    Initial commit from Catalyst

---
+++ build-config/schema.ts
@@ -0,0 +1,18 @@
+import { z } from 'zod';
+
+export const buildConfigSchema = z.object({
+  locales: z.array(
+    z.object({
+      code: z.string(),
+      isDefault: z.boolean(),
+      path: z.string().nullable(),
+    }),
+  ),
+  urls: z.object({
+    vanityUrl: z.string(),
+    cdnUrls: z.array(z.string()).default(['cdn11.bigcommerce.com']),
+    checkoutUrl: z.string(),
+  }),
+});
+
+export type BuildConfigSchema = z.infer<typeof buildConfigSchema>;
